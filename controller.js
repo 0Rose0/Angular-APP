ce4fe0d83590a3f08553d3aa6012b4db91a26fee
Kelly <captainkelly6@gmail.com>
2018-11-06 08:54:20 -0700

    Update controller.js

--- controller.js
+++ controller.js
@@ -1,3 +1,25 @@
 app.controller('MainController', ['$scope', function($scope) { 
-  $scope.title = 'Advnaced AI'; 
+  $scope.title = 'AI Aid'; 
+  $scope.promo = 'Come here for all your advanced AI needs!';
+  $scope.products = [
+    {
+      name: 'Maid';
+      modelNumber: '148-VSC';
+      functions: 'Cleaning the house';
+      miniDrones: 5;
+      price: 13000;
+      
+    },
+     {
+      name: 'Bodyguard';
+      modelNumber: '869-KXY';
+      functions: 'Protecting you and your assets';
+      miniDrones: 8;
+      price: 24000;
+     }
+    
+    
+    ]
+  
+  
 }]);
