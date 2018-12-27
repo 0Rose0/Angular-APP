eac5bb22c22683fcc8bc5b5743964f20b2c980d0
Kelly <captainkelly6@gmail.com>
2018-12-27 10:17:40 -0700

    Create appDirective.js

---
+++ appDirective.js
@@ -0,0 +1,9 @@
+app.directive('appInfo', function() { 
+  return { 
+    restrict: 'E', //means we're making a new html element
+    scope: { //scope specifies that we will pass information into this directive through an attribute named info. The = tells the directive to look for an attribute named info in the <app-info> element, like this:
+      info: '=' 
+    }, 
+    templateUrl: 'js/directives/appInfo.html' //templateUrl specifies the HTML to use in order to display the data in scope.info. Here we use the HTML in js/directives/appInfo.html.
+  }; 
+});
