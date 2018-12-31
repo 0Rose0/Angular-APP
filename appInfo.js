dac5db04c735ee6af3139580e1186df5eb9ec214
Kelly <captainkelly6@gmail.com>
2018-12-31 13:41:03 -0700

    Update appInfo.js

--- appInfo.js
+++ appInfo.js
@@ -4,6 +4,6 @@ app.directive('appInfo', function() {
     scope: { //scope specifies that we will pass information into this directive through an attribute named info. The = tells the directive to look for an attribute named info in the <app-info> element, like this:
       info: '=' 
     }, 
-    templateUrl: 'js/directives/appInfo.html' //templateUrl specifies the HTML to use in order to display the data in scope.info. Here we use the HTML in js/directives/appInfo.html.
+    templateUrl: 'appInfo.html' //templateUrl specifies the HTML to use in order to display the data in scope.info. Here we use the HTML in js/directives/appInfo.html.
   }; 
 });
