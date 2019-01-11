c50ad442ff76f26222720aba52162af0ff1458f7
Kelly <captainkelly6@gmail.com>
2019-01-11 10:55:00 -0700

    Update installApp.js

--- Directives/installApp.js
+++ Directives/installApp.js
@@ -5,6 +5,19 @@ app.directive('installApp',function(){
       info: '='
     },
   templateUrl:
-    'Directives/installApp.html'
+    'Directives/installApp.html',
+     link: function(scope, element, attrs){
+      scope.buttonText = "Install",
+        scope.download = function(){
+        element.toggleClass('btn-active');
+        if(scope.installed){
+          scope.buttonText = "Install";
+          scope.installed = false;
+        } else {
+          scope.buttonText = "Uninstall";
+          scope.installed = true;
+        }
+      }
+    }
   };
 });
