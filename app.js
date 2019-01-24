26c525bb2ca5326cf1133518d173e36edcdd9d29
Kelly <captainkelly6@gmail.com>
2019-01-24 08:35:51 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 var app=angular.module("myApp", []);
 
-app.controller('MainController', ['$scope', 'forecast', function($scope, forecast) {  
+app.controller('MainController', ['$scope', 'forecast', function($scope, forecast) { 
   forecast.success(function(data){
     $scope.fiveDay = data;
   });
+
   $scope.topTitle = 'AI Aid'; 
   $scope.promo = 'Come here for all your advanced AI needs!';
   $scope.products = [
