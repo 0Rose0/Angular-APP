1a31745152b89042bf30cd78d8ebd38c37a21d0c
Kelly <captainkelly6@gmail.com>
2019-03-18 08:58:46 -0600

    Update and rename app2.js to reroute.js

--- Apps/reroute.js
+++ Apps/reroute.js
@@ -1,6 +1,10 @@
 var app2 = angular.module('GalleryApp', ['ngRoute']);
 app.config(function($routeProvider){
   $routeProvider
+  .when('/',{
+    controller: 'MainController',
+    templateUrl: 'index.html'
+  })
   .when('/home/',{
     controller: 'HomeController',
     templateUrl: 'views/home.html'
