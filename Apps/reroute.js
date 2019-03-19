b093ce2df63b20deeb20654fb01a12fc107169d7
Kelly <captainkelly6@gmail.com>
2019-03-19 09:37:52 -0600

    Update reroute.js

--- Apps/reroute.js
+++ Apps/reroute.js
@@ -1,12 +1,12 @@
 var app2 = angular.module('GalleryApp', ['ngRoute']);
 app.config(function($routeProvider){
   $routeProvider
-  //.when('/',{
-//    controller: 'MainController',
-    //^^ this is messing things up, get rid of? change back to Home Controller?
- //   templateUrl: 'index.html'
-//  })
   .when('/',{
+   controller: 'MainController',
+    //^^ this is messing things up, get rid of? change back to Home Controller?
+    templateUrl: 'index.html'
+  })
+  .when('/home/',{
     controller: 'HomeController',
     templateUrl: 'views/home.html'
   })
